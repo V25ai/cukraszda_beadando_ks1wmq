0a254c4dcf6b32ec8abc253aad3a9bb1a839c0ae
V25ai <translucent105@gmail.com>
2026-05-06 23:45:30 +0200

    Customization of MVC layout and fix dynamic navigation rendering

--- views/page_main.php
+++ views/page_main.php
@@ -1,27 +1,50 @@
 <!DOCTYPE html>
-<html>
+<html lang="hu">
     <head>
         <meta charset="utf-8">
-        <title>MVC - PHP</title>
-        <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT?>css/main_style.css">
-        <?php if($viewData['style']) echo '<link rel="stylesheet" type="text/css" href="'.$viewData['style'].'">'; ?>
+        <title>Cukrászda Beadandó Weboldal</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT ?>css/main_style.css">
+        <?php 
+            if($viewData['style']) {
+                echo '<link rel="stylesheet" type="text/css" href="'.$viewData['style'].'">';
+            }
+        ?>
     </head>
     <body>
         <header>
-            <div id="user"><em><?= $_SESSION['userlastname']." ".$_SESSION['userfirstname'] ?></em></div>
-            <h1 class="header">Web-programozás II - MVC alkalmazás</h1>
+            <div id="user">
+                <?php
+                    if(isset($_SESSION['userlastname']) && isset($_SESSION['userfirstname'])) {
+                        echo '<em>Bejelentkezett: '
+                            . $_SESSION['userlastname']
+                            . ' '
+                            . $_SESSION['userfirstname']
+                            . '</em>';
+                    }
+                ?>
+            </div>
+
+            <h1 class="header">Váratlan Fordulat Cukrászda</h1>
         </header>
+
         <nav>
             <?php echo Menu::getMenu($viewData['selectedItems']); ?>
         </nav>
+
         <aside>
-            <a href="https://www.uni-neumann.hu" target="_blank" style="background-color:white; padding:0;"><img src="<?=SITE_ROOT?>\images\nje.png" width="150"></a><br>
-            <a href="https://neptun.uni-neumann.hu" target="_blank" style="background-color:white; padding:0;"><img src="<?=SITE_ROOT?>\images\neptun.png" width="150"></a><br>
-            <a href="https://gamf.uni-neumann.hu" target="_blank" style="background-color:white; padding:0;"><img src="<?=SITE_ROOT?>\images\gamf.png" width="150"></a><br>
+            <!-- Az eredeti NJE/GAMF képek eltávolítva. -->
         </aside>
+
         <section>
-            <?php if($viewData['render']) include($viewData['render']); ?>
+            <?php 
+                if($viewData['render']) {
+                    include($viewData['render']);
+                }
+            ?>
         </section>
-        <footer>&copy; NJE - GAMF - Informatika Tanszék <?= date("Y") ?></footer>
+
+        <footer>
+            &copy; Váratlan Fordulat Cukrászda <?= date("Y") ?>
+        </footer>
     </body>
 </html>
